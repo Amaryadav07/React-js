615c13350fd6a3f017f54381c28e8e5acd6dfaac
Amaryadav07 <anath2893007@gmail.com>
2025-03-27 22:16:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+// const App=()=>{
+// return(
+// <>
+//     <h1> Welcome to React js</h1>
+//     <h2>This is JSX not a HTML</h2>
+//     <marquee behavior="scroll" direction="left"> <h1>I am a MERN Stack Developer</h1></marquee>
+//     <form action="" method="post">
+//     <fieldset>
+//         <legend>Form Details</legend>
+//         <br />
+//         <label for="name">Name:</label>
+//         <input type="text" id="name" name="name" required /><br /><br />
+        
+//         <label for="email">Email:</label>
+//         <input type="email" id="email" name="email" required /><br /><br />
+        
+//         <label for="mobile">Mobile:</label>
+//         <input type="number" id="mobile" name="mobile" inputmode="numeric" required /><br /><br />
+        
+//         <label for="password">Password:</label>
+//         <input type="password" id="password" name="password" required /><br /><br />
+        
+//         <button type="submit">Signup</button>
+//     </fieldset>
+// </form>
+
+//     </>
+//   )
+//   }
+// export default App;
+
+// const user = {
+//   name: 'Hedy Lamarr',
+//   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+//   imageSize: 90,
+// };
+
+// export default function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className="avatar"
+//         src={user.imageUrl}
+//         alt={'Photo of ' + user.name}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+
+
+// const name="amar"
+// const age=25
+// const salary=10000
+
+// const App=()=>{
+
+// return(
+
+// <p>welcome to react i am {name} my age is {age} salary is {salary}</p>
+
+// )
+
+// }
+// export default App;
+
+// LARGE BLOCK OF CODE WILL BE WRITTEN AS BELOW-----------------------------------------------------
+// const subject=<ul>
+//     <li>JAVA</li>
+//     <li>PHP</li>
+//     <li>MERN</li>
+//     <li>REACT</li>
+// </ul>
+
+
+// const App=()=>{
+
+// return(
+
+// <h1>My Subject: {subject}</h1>
+
+// )
+
+// }
+// export default App;
+
+// ONE TOP LEVEL ELEMENT----------------------------------------------
+
+const App=()=>{
+
+return(
+
+<>
+<h1>hii i am amar</h1>
+<h2>mern developer</h2>
+
+</>
+)
+
+}
+export default App;
